8fdb3e79d4d342616b3bd4ff7f324decb5ee132b
Rifky Alfian <rifky@nagaexchange.co.id>
2026-02-13 18:11:52 +0700

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WEB\GuestBookController;
 
-Route::get('/', [GuestBookController::class, 'index'])->name('guest-book.index');
-Route::post('/store', [GuestBookController::class, 'store'])->name('guest-book.store'); // Rute baru
+
+Route::redirect('/', '/buku-tamu');
+
+// Route::get('/', [GuestBookController::class, 'index'])->name('guest-book.index');
+// Route::post('/store', [GuestBookController::class, 'store'])->name('guest-book.store'); // Rute baru
+
+Route::get('buku-tamu', [GuestBookController::class, 'index'])->name('guest-book.index');
+Route::post('buku-tamu/store', [GuestBookController::class, 'store'])->name('guest-book.store'); // Rute baru
